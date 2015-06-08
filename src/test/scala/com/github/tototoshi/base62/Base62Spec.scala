30251f957553200a4c50d2bd99f53a7a0e538405
seanbo <sbollin@societyconsulting.com>
2015-06-07 19:30:08 -0700

    added simple tests for base62 conversion

---
+++ src/test/scala/com/github/tototoshi/base62/Base62Spec.scala
@@ -0,0 +1,18 @@
+import org.scalatest._
+import com.github.tototoshi.base62._
+
+class Base62Spec extends FlatSpec with Matchers {
+  it should "convert 1 to 1" in {
+    val base62 = new Base62
+    val encoded = base62.encode(1)
+
+    encoded shouldBe "1"
+  }
+
+  it should "convert 12432 to 3EW" in {
+    val base62 = new Base62
+    val encoded = base62.encode(12432)
+
+    encoded shouldBe "3EW"
+  }
+}
